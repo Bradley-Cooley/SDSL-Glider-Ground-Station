f957be7d736717b6ae3abde3a6c360ddb292ba82
Bradley-Cooley <snufflestheanteater@gmail.com>
2015-10-23 18:47:55 -0700

    Initial commit.
    Designer stuff with basic layout of containers.

---
+++ SDSL-Glider-Ground-Station/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace SDSL_Glider_Ground_Station
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tableLayoutMain = new System.Windows.Forms.TableLayoutPanel();
+            this.missionInfoGroup = new System.Windows.Forms.GroupBox();
+            this.imageGroup = new System.Windows.Forms.GroupBox();
+            this.missionControlGroup = new System.Windows.Forms.GroupBox();
+            this.tableLayoutSmallGraphs = new System.Windows.Forms.TableLayoutPanel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart5 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tableLayoutMain.SuspendLayout();
+            this.tableLayoutSmallGraphs.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutMain
+            // 
+            this.tableLayoutMain.ColumnCount = 3;
+            this.tableLayoutMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutMain.Controls.Add(this.missionInfoGroup, 0, 0);
+            this.tableLayoutMain.Controls.Add(this.imageGroup, 1, 0);
+            this.tableLayoutMain.Controls.Add(this.missionControlGroup, 0, 1);
+            this.tableLayoutMain.Controls.Add(this.tableLayoutSmallGraphs, 2, 0);
+            this.tableLayoutMain.Controls.Add(this.chart5, 2, 1);
+            this.tableLayoutMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutMain.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.tableLayoutMain.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutMain.Name = "tableLayoutMain";
+            this.tableLayoutMain.RowCount = 2;
+            this.tableLayoutMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutMain.Size = new System.Drawing.Size(1301, 738);
+            this.tableLayoutMain.TabIndex = 0;
+            // 
+            // missionInfoGroup
+            // 
+            this.missionInfoGroup.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.missionInfoGroup.Location = new System.Drawing.Point(3, 3);
+            this.missionInfoGroup.Name = "missionInfoGroup";
+            this.missionInfoGroup.Size = new System.Drawing.Size(427, 363);
+            this.missionInfoGroup.TabIndex = 0;
+            this.missionInfoGroup.TabStop = false;
+            this.missionInfoGroup.Text = "Mission Info";
+            this.missionInfoGroup.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // imageGroup
+            // 
+            this.imageGroup.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageGroup.Location = new System.Drawing.Point(436, 3);
+            this.imageGroup.Name = "imageGroup";
+            this.tableLayoutMain.SetRowSpan(this.imageGroup, 2);
+            this.imageGroup.Size = new System.Drawing.Size(427, 732);
+            this.imageGroup.TabIndex = 1;
+            this.imageGroup.TabStop = false;
+            this.imageGroup.Text = "Imaging";
+            // 
+            // missionControlGroup
+            // 
+            this.missionControlGroup.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.missionControlGroup.Location = new System.Drawing.Point(3, 372);
+            this.missionControlGroup.Name = "missionControlGroup";
+            this.missionControlGroup.Size = new System.Drawing.Size(427, 363);
+            this.missionControlGroup.TabIndex = 2;
+            this.missionControlGroup.TabStop = false;
+            this.missionControlGroup.Text = "Mission Control";
+            // 
+            // tableLayoutSmallGraphs
+            // 
+            this.tableLayoutSmallGraphs.ColumnCount = 2;
+            this.tableLayoutSmallGraphs.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutSmallGraphs.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutSmallGraphs.Controls.Add(this.chart1, 0, 0);
+            this.tableLayoutSmallGraphs.Controls.Add(this.chart2, 1, 0);
+            this.tableLayoutSmallGraphs.Controls.Add(this.chart3, 0, 1);
+            this.tableLayoutSmallGraphs.Controls.Add(this.chart4, 1, 1);
+            this.tableLayoutSmallGraphs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutSmallGraphs.Location = new System.Drawing.Point(869, 3);
+            this.tableLayoutSmallGraphs.Name = "tableLayoutSmallGraphs";
+            this.tableLayoutSmallGraphs.RowCount = 2;
+            this.tableLayoutSmallGraphs.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutSmallGraphs.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutSmallGraphs.Size = new System.Drawing.Size(429, 363);
+            this.tableLayoutSmallGraphs.TabIndex = 3;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(3, 3);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(208, 175);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(217, 3);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(209, 175);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            this.chart3.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend3.Enabled = false;
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(3, 184);
+            this.chart3.Name = "chart3";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart3.Series.Add(series3);
+            this.chart3.Size = new System.Drawing.Size(208, 176);
+            this.chart3.TabIndex = 2;
+            this.chart3.Text = "chart3";
+            // 
+            // chart4
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart4.ChartAreas.Add(chartArea4);
+            this.chart4.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend4.Enabled = false;
+            legend4.Name = "Legend1";
+            this.chart4.Legends.Add(legend4);
+            this.chart4.Location = new System.Drawing.Point(217, 184);
+            this.chart4.Name = "chart4";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart4.Series.Add(series4);
+            this.chart4.Size = new System.Drawing.Size(209, 176);
+            this.chart4.TabIndex = 3;
+            this.chart4.Text = "chart4";
+            // 
+            // chart5
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart5.ChartAreas.Add(chartArea5);
+            this.chart5.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend5.Enabled = false;
+            legend5.Name = "Legend1";
+            this.chart5.Legends.Add(legend5);
+            this.chart5.Location = new System.Drawing.Point(869, 372);
+            this.chart5.Name = "chart5";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart5.Series.Add(series5);
+            this.chart5.Size = new System.Drawing.Size(429, 363);
+            this.chart5.TabIndex = 4;
+            this.chart5.Text = "chart5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1301, 738);
+            this.Controls.Add(this.tableLayoutMain);
+            this.Name = "Form1";
+            this.Text = "SDSL Glider Ground Station";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tableLayoutMain.ResumeLayout(false);
+            this.tableLayoutSmallGraphs.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutMain;
+        private System.Windows.Forms.GroupBox missionInfoGroup;
+        private System.Windows.Forms.GroupBox imageGroup;
+        private System.Windows.Forms.GroupBox missionControlGroup;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutSmallGraphs;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart5;
+
+    }
+}
+
